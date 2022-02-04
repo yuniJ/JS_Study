cebf6642df26cae7f6212b5a279f8588da4e7df2
alandscape9 <alandscape9@gmail.com>
2022-02-04 11:21:22 +0900

    ex3

---
+++ src/ex-3.js
@@ -0,0 +1,17 @@
+import { question } from 'readline-sync';
+
+// 문제 3
+// 'What do you call a bear with no teeth?'라는 농담을 표시하고,
+// 다음줄에 'A gummy bear!'라는 답을 표시하는 코드를 한줄로 만들자.
+
+// 두 줄의 문장을 한 구문(명령어)으로 출력해보자.
+// 힌트 : js escape character 로 검색
+
+// 변수2개 저장
+// 줄바꿈 출력
+
+const sentence = "' What do you call a bear with no teeth?' \n'A gummy bear!'";
+
+const output = sentence;
+
+console.log(output);
